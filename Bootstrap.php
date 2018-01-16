8b9067b2a966343703974a5495ef576df211d2c5
Yuri Basov <w3devlab@gmail.com>
2018-01-15 20:31:45 -0800

    namespace fix. Added auto bootstrap

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\modules\yii2x\user\ui\ext;
+namespace \yii2x\user\ui\ext;
 
 use \yii\base\BootstrapInterface;
 
--- Module.php
+++ Module.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\modules\yii2x\user\ui\ext;
+namespace \yii2x\user\ui\ext;
 
 /**
  * enterprise module definition class
@@ -22,36 +22,3 @@ class Module extends \yii\base\Module
         // custom initialization code goes here
     }
 }
-
-    "repositories":[
-        {
-            "type": "git",
-            "url": "https://github.com/yii2x/yii2-enterprise.git"
-        },
-        {
-            "type": "git",
-            "url": "https://github.com/yii2x/yii2-filesystem.git"
-        },                
-        {
-            "type": "git",
-            "url": "https://github.com/yii2x/yii2-user.git"
-        },
-        {
-            "type": "git",
-            "url": "https://github.com/yii2x/yii2-user-ui-ext.git"
-        },
-        {
-            "type": "git",
-            "url": "https://github.com/yii2x/yii2-user-ui-admin-lte.git"
-        },
-        {
-            "type": "git",
-            "url": "https://github.com/yii2x/yii2-rbac.git"
-        },
-        {
-            "type": "git",
-            "url": "https://github.com/yii2x/yii2-abac.git"
-        },                
-    ], 
-                
-                composer show yii2x/yii2-abac
